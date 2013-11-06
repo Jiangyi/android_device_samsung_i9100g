6015c748e6b61e33464a6bc2c56fbf7d18025287
Jiangyi <sam.andrew.jiang@gmail.com>
2013-11-06 11:39:05 -0500

    Actually copy wpa/p2p supplicant overlays

--- device.mk
+++ device.mk
@@ -47,7 +47,8 @@ PRODUCT_COPY_FILES += \
 
 # Wifi
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf
+    $(LOCAL_PATH)/configs/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf \
+    $(LOCAL_PATH)/configs/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf
 
 PRODUCT_PROPERTY_OVERRIDES += \
     wifi.interface=wlan0 \
